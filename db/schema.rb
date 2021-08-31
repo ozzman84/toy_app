96c052a08a5c6b2e7b1695982e8f3485343085ab
Ozzie <mikeosmonson@gmail.com>
2021-08-30 20:35:55 -0600

    Add microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_003916) do
+ActiveRecord::Schema.define(version: 2021_08_31_012708) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
